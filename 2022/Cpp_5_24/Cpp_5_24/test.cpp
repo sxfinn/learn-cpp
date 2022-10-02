63aa413edb5e73d2a8fa615ac8817987f7a0a42e
sxfinn <sxnicoa@gmail.com>
2022-10-02 10:42:08 +0800

    Code submitted today

--- 2022/Cpp_5_24/Cpp_5_24/test.cpp
+++ 2022/Cpp_5_24/Cpp_5_24/test.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 
 int main()
 {
-	for (int i = 0; i < 1000; i++)
-	{
-		if ((i & (~6)) == (((i ^ 6) ^ (i ^ 6)) & 6))
-			;
-		else
-		{
-			printf("error\n");
-		}
-	}
+	string s("hello");
 	return 0;
 }
+
+//int main()
+//{
+//	for (int i = 0; i < 1000; i++)
+//	{
+//		if ((i & (~6)) == (((i ^ 6) ^ (i ^ 6)) & 6))
+//			;
+//		else
+//		{
+//			printf("error\n");
+//		}
+//	}
+//	return 0;
+//}
